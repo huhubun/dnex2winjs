7f036ebeab7060a7b3fe861abc74ad5a9ef4bde4
Zhichao Nong <nongzc@outlook.com>
2016-03-05 14:29:04 +0800

    Initial commit

---
+++ dnex2winjs/dnex2winjs.Test/DNEXTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using System.Reflection;
+using Xunit;
+
+namespace dnex2winjs.Test
+{
+    public class DNEXTest
+    {
+        [Fact]
+        public void GetAllExceptionsTest()
+        {
+            // Arrange
+            var assembly = Assembly.Load("mscorlib");
+            var allExceptions = assembly.GetTypes().Where(t => t.IsPublic)
+                                                .Where(t => t.Name != "_Exception")
+                                                .Where(t => t.Name.EndsWith("Exception"))
+                                                .Select(t => t.Name);
+
+            // Act
+            var result = DNEX.GetAllExceptions();
+
+            // Assert
+            Assert.Equal(allExceptions.Count(), result.Count);
+            Assert.Equal(allExceptions.Intersect(result.Keys).Count(), result.Count);
+        }
+    }
+}
---
+++ dnex2winjs/dnex2winjs/DNEX.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace dnex2winjs
+{
+    public static class DNEX
+    {
+        static readonly Type TYPE_OF_EXCEPTION = typeof(Exception);
+        static readonly Assembly assembly = Assembly.Load("mscorlib");
+
+        /// <summary>
+        /// 获取 .NET Framework 定义的所有异常的名称和 HResult
+        /// </summary>
+        /// <returns></returns>
+        public static Dictionary<string, int> GetAllExceptions()
+        {
+            var result = new Dictionary<string, int>();
+
+            // 获取 .NET Framework 定义的所有 Exception 类
+            var allExceptions = assembly.GetTypes().Where(t => t.IsClass && t.IsPublic && (t.IsSubclassOf(TYPE_OF_EXCEPTION) || t.Equals(TYPE_OF_EXCEPTION)));
+
+            foreach (var exceptionType in allExceptions)
+            {
+                Exception ex = null;
+
+                try
+                {
+                    // 使用无参构造函数初始化
+                    ex = assembly.CreateInstance(exceptionType.Namespace + "." + exceptionType.Name, false, BindingFlags.Default | BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public, null, null, null, null) as Exception;
+                }
+                catch
+                {
+                    // 对于没有无参构造函数的异常类（例如 RuntimeWrappedException）
+                    // 使用一个参数，类型为 Object 的构造函数初始化
+                    ex = assembly.CreateInstance(exceptionType.Namespace + "." + exceptionType.Name, false, BindingFlags.Default | BindingFlags.Instance | BindingFlags.NonPublic, null, new object[] { new object() }, null, null) as Exception;
+                }
+
+                result.Add(exceptionType.Name, ex.HResult);
+            }
+
+            return result;
+        }
+
+        public static Dictionary<string, int> GetAllHResults()
+        {
+            var result = new Dictionary<string, int>();
+
+            var HResultsClass = assembly.GetTypes().First(t => t.Name.Equals("__HResults"));
+
+            foreach (var item in HResultsClass.GetRuntimeFields())
+            {
+                result.Add(item.Name, (int)item.GetValue(null));
+            }
+
+            return result;
+        }
+    }
+}
