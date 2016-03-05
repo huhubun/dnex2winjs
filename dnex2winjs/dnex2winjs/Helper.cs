de3b6df64cf9cd12123084ddababb42af4fdc155
Zhichao Nong <nongzc@outlook.com>
2016-03-05 20:10:43 +0800

    Add a method to get the version number, and displayed into dnex.js's template

---
+++ dnex2winjs/dnex2winjs/Helper.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+namespace dnex2winjs
+{
+    public static class Helper
+    {
+        public static string GetVersion()
+        {
+            return Assembly.GetExecutingAssembly().GetName().Version.ToString();
+        }
+    }
+}
--- dnex2winjs/dnex2winjs/Properties/AssemblyInfo.cs
+++ dnex2winjs/dnex2winjs/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("dnex2winjs")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Exception mapping of .NET Framework to WinJS")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("dnex2winjs")]
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
